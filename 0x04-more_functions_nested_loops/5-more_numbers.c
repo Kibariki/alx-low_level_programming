783dc592f67f315dc88daeb5df56d8fe9f458d60
steppahyadih <simonndungu755@gmail.com>
2023-07-20 12:47:19 -0700

    yadih

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,40 @@
+/* main.h (header file) */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void);
+
+#endif /* MAIN_H */
+
+
+/* 5-more_numbers.c (source file) */
+
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
