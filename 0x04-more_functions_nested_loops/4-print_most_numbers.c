34a4fafab841b266dfcfb4a0eb8e79c119d6fe95
steppahyadih <simonndungu755@gmail.com>
2023-07-20 22:55:59 -0700

    yadih

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,36 +1,20 @@
-/* main.h (header file) */
+#include "main.h"
 
-#ifndef MAIN_H
-#define MAIN_H
-
-/* Function prototype for _putchar */
+/* Prototype declaration for _putchar */
 int _putchar(char c);
 
 /**
- * print_most_numbers - Prints the numbers from 0 to 9,
- * excluding 2 and 4, followed by a new line.
- */
-void print_most_numbers(void);
-
-#endif /* MAIN_H */
-
-
-/* 4-print_most_numbers.c (source file) */
-
-#include "main.h"
-
-/**
- * print_most_numbers - Prints the numbers from 0 to 9,
- * excluding 2 and 4, followed by a new line.
+ * print_most_numbers - prints 01356789
+ * Return: void
  */
 void print_most_numbers(void)
 {
-	char number;
+	char c;
 
-	for (number = '0'; number <= '9'; number++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (number != '2' && number != '4')
-			_putchar(number);
+		if (c != '2' && c != '4')
+			_putchar(c);
 	}
 	_putchar('\n');
 }
